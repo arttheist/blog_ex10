4fea1a0bfb8a56a673b1b848c5f7083f41aee441
Rene Vos <re.vos@arttheisten.de>
2014-01-30 21:08:54 +0100

    Setting up First Test

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -3,8 +3,9 @@ require 'spec_helper'
 describe "Articles" do
   describe "GET /articles" do
     it "display some articles" do
-      visit articles_path
-      page.should have_content 'lorem ipsum'
+    	@article = Article.create :title => 'First Article', :author => 'René Vos', :body => 'lorem ipsum'
+    	visit articles_path
+    	page.should have_content 'lorem ipsum'
     end
   end
 end
